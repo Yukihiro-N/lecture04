1004e1aa9cae945ffec4cd1e4a773686e1e978ab
Yukihiro NAKAJIMA <s13611yn@sfc.keio.ac.jp>
2014-05-01 10:44:07 +0900

    フォクスケを2個に追加(click,mouseover)

--- js/slidein.js
+++ js/slidein.js
@@ -4,4 +4,12 @@ var slideinFoxkeh = function(){
 };
 
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
-slideinFoxkehButton.addEventListener("mouseover", slideinFoxkeh);
+slideinFoxkehButton.addEventListener("click", slideinFoxkeh);
+
+var mouseoverFoxkeh = function(){
+	var foxkeh = document.querySelector("#mouseover-foxkeh img");
+	foxkeh.setAttribute("class", "mouseover");
+};
+
+var mouseoverFoxkehButton = document.querySelector("#mouseover-foxkeh button");
+mouseoverFoxkehButton.addEventListener("mouseover", mouseoverFoxkeh);
